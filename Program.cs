f5a8215b97f60b8c394ceb93d52de1ed93771f2c
keichains <tqkhai.contact@gmail.com>
2024-03-22 08:51:07 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace int32
+{
+    public class IntStruct
+    {
+        static void Main()
+        {
+            int num; string snum;
+            Console.Write("Enter a number: "); snum = Console.ReadLine(); num = Int32.Parse(snum);
+            Console.WriteLine(num);
+        }
+
+    }
+}
